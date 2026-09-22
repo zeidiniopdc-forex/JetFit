e2bf8a3a9a9654b7fee241e05489609f8c8b112e
zeidiniopdc-forex <zeidi.niopdc@gmail.com>
2026-09-22 07:33:36 +0330

    Add ViewModel Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
